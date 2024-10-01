3d09db96cd756be4d94082f3a709ccda346751e5
David Bauch <jamesbauch@gmail.com>
2024-09-30 23:01:59 -0600

    added base path of repo dir to build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { dirname, basename } from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module (the parent directory)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const basePath = basename(__dirname);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +15,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: `/${basePath}`,
+		},
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
